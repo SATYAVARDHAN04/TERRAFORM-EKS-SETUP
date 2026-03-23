bc90815ab603e5f7918313dabb94178c7235d38d
SATYAVARDHAN04 <svardhan278@gmail.com>
2026-03-23 17:44:30 +0530

    k8s

--- 10-sg/parameter.tf
+++ 10-sg/parameter.tf
@@ -1,19 +1,7 @@
-resource "aws_ssm_parameter" "sg-id" {
-  name  = "/${var.project}/${var.environment}/frontend_sg"
-  type  = "String"
-  value = module.frontend_sg.sg_id
-}
-
 resource "aws_ssm_parameter" "bastion-sg" {
   name  = "/${var.project}/${var.environment}/bastionsg"
   type  = "String"
   value = module.bastion.sg_id
-}
-
-resource "aws_ssm_parameter" "alb-sg" {
-  name  = "/${var.project}/${var.environment}/albsg"
-  type  = "String"
-  value = module.backend_alb.sg_id
 }
 
 resource "aws_ssm_parameter" "vpn-sg" {
@@ -22,62 +10,20 @@ resource "aws_ssm_parameter" "vpn-sg" {
   value = module.vpn_sg.sg_id
 }
 
-resource "aws_ssm_parameter" "mongodb-sg" {
-  name  = "/${var.project}/${var.environment}/mongodb_sg"
+resource "aws_ssm_parameter" "ingress" {
+  name  = "/${var.project}/${var.environment}/ingress"
   type  = "String"
-  value = module.mongodb_sg.sg_id
+  value = module.ingress.sg_id
 }
 
-resource "aws_ssm_parameter" "redis-sg" {
-  name  = "/${var.project}/${var.environment}/redis_sg"
+resource "aws_ssm_parameter" "control_plane" {
+  name  = "/${var.project}/${var.environment}/control_plane"
   type  = "String"
-  value = module.redis_sg.sg_id
+  value = module.control_plane.sg_id
 }
 
-resource "aws_ssm_parameter" "rabbitmq-sg" {
-  name  = "/${var.project}/${var.environment}/rabbitmq_sg"
+resource "aws_ssm_parameter" "nodes" {
+  name  = "/${var.project}/${var.environment}/nodes"
   type  = "String"
-  value = module.rabbitmq_sg.sg_id
-}
-
-resource "aws_ssm_parameter" "mysql-sg" {
-  name  = "/${var.project}/${var.environment}/mysql_sg"
-  type  = "String"
-  value = module.mysql_sg.sg_id
-}
-
-resource "aws_ssm_parameter" "catalogue-sg" {
-  name  = "/${var.project}/${var.environment}/catalogue_sg"
-  type  = "String"
-  value = module.catalogue_sg.sg_id
-}
-
-resource "aws_ssm_parameter" "cart-sg" {
-  name  = "/${var.project}/${var.environment}/cart_sg"
-  type  = "String"
-  value = module.cart_sg.sg_id
-}
-
-resource "aws_ssm_parameter" "user-sg" {
-  name  = "/${var.project}/${var.environment}/user_sg"
-  type  = "String"
-  value = module.user_sg.sg_id
-}
-
-resource "aws_ssm_parameter" "shipping-sg" {
-  name  = "/${var.project}/${var.environment}/shipping_sg"
-  type  = "String"
-  value = module.shipping_sg.sg_id
-}
-
-resource "aws_ssm_parameter" "payment-sg" {
-  name  = "/${var.project}/${var.environment}/payment_sg"
-  type  = "String"
-  value = module.payment_sg.sg_id
-}
-
-resource "aws_ssm_parameter" "frontend-alb" {
-  name  = "/${var.project}/${var.environment}/frontendalb_sg"
-  type  = "String"
-  value = module.frontend_alb.sg_id
+  value = module.nodes.sg_id
 }
